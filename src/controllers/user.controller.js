473f01253f2b816820f2fab51ae3de92d87e136b
NileshMargaj <nileshmargaj751@gmail.com>
2026-03-31 11:15:25 +0530

    refactor user controller and subscription model for improved structure and functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -235,10 +235,167 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 })
 
+
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(401, "Old password is incorrect");
+    }
+
+    user.password = newPassword
+    await user.save({ validateBeforeSave: false })
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200, {}, "Password changed successfully..."
+            )
+        )
+})
+
+
+const getCurrerntUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { user: req.user },
+                "Current user fetched successfully..."
+            )
+        )
+})
+
+
+const updateUserDetails = asyncHandler(async (req, res) => {
+    const { fullName, email } = req.body
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                fullName,
+                email
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                { user },
+                "User details updated successfully..."
+            )
+        )
+
+})
+
+
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    const avatarLocalPath = req.file?.path
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing");
+    }
+
+    const avatar = await uploadOnImageKit(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new ApiError(500, "Failed to upload avatar");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            { user },
+            "User avatar updated successfully..."
+        )
+    )
+})
+
+
+
+const updateCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image file is missing");
+    }
+
+    const coverImage = await uploadOnImageKit(coverImageLocalPath)
+
+    if (!coverImage.url) {
+        throw new ApiError(500, "Failed to upload cover image");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            { user },
+            "User Cover image updated successfully..."
+        )
+    )
+})
+
+
+
+
+
+
+
+
 export {
     registerUser,
     loginUser,
     logOutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrerntUser,
+    updateUserDetails,
+    updateUserAvatar,
+    updateCoverImage
 };
 
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
 const subscriptionSchema = new mongoose.Schema({
-subscriber :{
-    typeof: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true
-},
-channel :{
-    typeof: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true  }
-},{timestamps: true});
+    subscriber: {
+        typeof: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    channel: {
+        typeof: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, { timestamps: true });
 
 const Subscription = mongoose.model("Subscription", subscriptionSchema);
 
